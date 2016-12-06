3437ec645d30bc10dea1d8051a89977f7633899d
Craig Duncan <craig@CraigMBPR.local>
2016-12-06 12:27:10 +0000

    Created project structure and initial dependencies

---
+++ src/test/scala/uk/co/testcraft/restfulbookerplatform/tags/Tags.scala
@@ -0,0 +1,5 @@
+package uk.co.testcraft.restfulbookerplatform.tags
+
+import org.scalatest.Tag
+
+object SmokeTest extends Tag("uk.co.testcraft.restfulbookerplatform.tags.SmokeTest")
---
+++ src/test/scala/uk/co/testcraft/restfulbookerplatform/tests/BaseFeatureSpec.scala
@@ -0,0 +1,7 @@
+package uk.co.testcraft.restfulbookerplatform.tests
+
+import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
+
+trait BaseFeatureSpec extends FeatureSpec with GivenWhenThen with Matchers {
+
+}
